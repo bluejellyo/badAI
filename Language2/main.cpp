a3f3d1e2522ccbbd3bacfa59cde1499da984e0a5
Samuel Humphreys <samuelhumphreys@Samuels-MacBook-Pro.local>
2014-08-24 12:13:45 +0100

    Initial Commit

---
+++ Language2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Language2
+//
+//  Created by Samuel Humphreys on 24/08/2014.
+//  Copyright (c) 2014 Samuel Humphreys. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
